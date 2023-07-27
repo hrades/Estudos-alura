8762d7ac73b7c6d41f952a9861a7ab351ae1e15a
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2023-07-27 14:42:26 -0300

    Ping Pong finalizado
    Finalização do código do jogo, adição de sons e melhora de performance

--- Pong_JS/sketch.js
+++ Pong_JS/sketch.js
@@ -10,7 +10,7 @@ let rBola = dBola/2;
 
 //Tamanhos das raquetes
 let larRaq = 10;
-let altRaq = 90;
+let altRaq = 80;
 
 //Posições das raquetes
 let xRaqEsq = 5;
@@ -21,16 +21,29 @@ let yRaqDir = (altura/2) - (altRaq/2)
 //Velocidade
 let velocXBola = 6;
 let velocYBola = 6;
-let velocRaq = 6;
+let velocRaq = 8;
 let velocOpo;
 
 //Variáveis de controle
 let colidiu = false;
 let pontosA = 0; //Jogador azul
 let pontosV = 0; //Jogador vermelho
+let errar = 0;
+
+//Sons
+let smash;
+let ponto;
+let theme;
+
+function preload(){
+  smash = loadSound("raquetada.wav");
+  ponto = loadSound("Coin.wav");
+  theme = loadSound("Som de fundo.wav");
+}
 
 function setup() {
   createCanvas(largura, altura); //Tamanho da tela
+  theme.loop();
 }
 
 function draw() {
@@ -43,6 +56,8 @@ function draw() {
   detectaRaquetes();
   placar();
   pontuar();
+  naoPrende();
+  
 }
 
 function showComponentes(){
@@ -50,7 +65,7 @@ function showComponentes(){
   fill('white')
   circle(xBola, yBola, dBola);
   //Raquete 
-  fill('blue');
+  fill(color(0,255,255));
   rect(xRaqEsq, yRaqEsq, larRaq, altRaq);
   //Raquete vermelha
   fill('red')
@@ -85,8 +100,19 @@ function moveRaqEsq(){
 }
 
 function moveRaqDir(){
-  velocOpo = yBola - yRaqDir - altRaq/ 2 - 35;
+  //JOGO SOLO
+  velocOpo = yBola - yRaqDir - altRaq/ 2 - errar;
   yRaqDir += velocOpo;
+  chanceErrar();
+  
+  //2 PLAYERS -- apagar /**/ para jogar
+  /*if(keyIsDown(87)){ //87 é o valor deswsw W
+    yRaqDir -= 10;
+  }
+  else if(keyIsDown(83)){ //83 é o valor de S
+    yRaqDir += 10;
+  }*/
+  
   //Limitar área de movimentação
   yRaqDir = constrain(yRaqDir, 10, 300);
 }
@@ -95,27 +121,51 @@ function detectaRaquetes(){
   //Colidiu com a raquete azul
   if((xBola - rBola) < (xRaqEsq+larRaq) && (yBola - rBola) < (yRaqEsq + altRaq) && (yBola + rBola) > yRaqEsq){
     velocXBola *= -1;
+    smash.play();
   }
   
   //Colidiu com a raquete vermelha (usando biblioteca)
   colidiu = collideRectCircle(xRaqDir, yRaqDir, larRaq, altRaq, xBola, yBola, rBola);
   if(colidiu){
     velocXBola *= -1;
+    smash.play();
   }
   
 }
 
 function placar(){
-  fill('orange');
-  text(pontosA, 200, 26);
-  text(pontosV, 400, 26)
+  textAlign(CENTER);
+  textSize(20);
+  fill(color(0,255,255))
+  text(pontosA, 150, 26);
+  fill('red')
+  text(pontosV, 450, 26)
 }
 
 function pontuar(){
   if(xBola > 590){
     pontosA += 1;
+    ponto.play();
   }
   if(xBola < 10){
     pontosV += 1;
+    ponto.play();
   }
+}
+
+function chanceErrar(){
+  if(pontosV > pontosA)
+  {
+    errar = 92;
+  }
+  if(pontosV < pontosA && errar > 50)
+  {
+    errar -= 2;
+  }
+}
+
+function naoPrende(){
+    if (xBola - rBola < 0){
+    xBola = 23
+    }
 }
